368391390ba5d25e8816e853224acf3f80261f9e
Shubham negi <shubhamnegi1878@gmail.com>
2025-09-27 19:54:37 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,29 +5,17 @@ import random
 -1 for water 
 0  for gun
 '''
-computer = random.choice([-1,0,1])
-youstr = input("Enter your choice:")
-youDict={"s":1,"w":-1,"g":0}
-reverseDict ={1:"Snake",-1:"Water",0:"Gun"}
+computer = random.choice([-1, 0, 1])
+youstr = input("Enter your choice (s/w/g): ").lower()
+youDict = {"s": 1, "w": -1, "g": 0}
+reverseDict = {1: "Snake", -1: "Water", 0: "Gun"}
 
-you= youDict[youstr]
-print(f"You choose {reverseDict[you]}\nComputer choose {reverseDict[computer]}")
+you = youDict[youstr]
+print(f"You chose {reverseDict[you]}\nComputer chose {reverseDict[computer]}")
 
-if(computer == you):
-    print("Its a draw")
-
+if computer == you:
+    print("It's a draw")
+elif (you == 1 and computer == -1) or (you == -1 and computer == 0) or (you == 0 and computer == 1):
+    print("You win!")
 else:
-        if(computer ==-1 and you ==1):
-            print("You win")
-        elif(computer ==-1 and you==0):
-             print("You lose")
-        elif(computer ==1 and you ==-1):
-             print("You win")
-        elif(computer ==0 and you==1):
-             print("You lose")
-        else:
-             print("Something is wrong")
-   
-
-        
-    
+    print("You lose!")
